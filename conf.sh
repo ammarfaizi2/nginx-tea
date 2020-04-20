f7508b6c8cf123cc0ac1dd022904e97a90610ca9
Ammar Faizi <ammarfaizi2@gmail.com>
2020-04-20 19:13:38 +0700

    Clean up

--- conf.sh
+++ conf.sh
@@ -1,2 +1,10 @@
-cp -vf $(pwd)/myconf/nginx.conf /etc/nginx
-cp -vf $(pwd)/
+
+for i in $(ls myconf/snippets/); do
+  rm -vf /etc/nginx/conf/$i;
+done;
+
+cp -vfr $(pwd)/myconf/sites-enabled /etc/nginx;
+cp -vfr $(pwd)/myconf/snippets /etc/nginx/conf;
+cp -vf  $(pwd)/myconf/nginx.conf /etc/nginx/conf;
+# cp -vf  $(pwd)/nginx.service /lib/systemd/system;
+# systemctl daemon-reload;
